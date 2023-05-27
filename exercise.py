76c0ced3f761a4865102fcff1295b5324e0bdb5e
Emilio Romo <natsutsu04@gmail.com>
2023-05-26 20:32:06 -0600

    mensaje

---
+++ exercise.py
@@ -0,0 +1,14 @@
+#We print the default data
+print("Civilian data:")
+print("Name: Samuel" )
+print("Nationality: Boliviano")
+print("Age: 18")
+#Now we introduce the new data
+name = input("Enter the new name: ")
+nationality = input("Enter the new nationality: ")
+age = int(input("Enter the new age: "))
+#Now the code will show the new data that has been entered
+print("Final data:")
+print("Name:", name)
+print("Nationality:", nationality)
+print("Age:", age)
