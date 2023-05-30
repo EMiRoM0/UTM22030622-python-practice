2ce6cfe6fefc675b0b2b00435b97d390b6db97f4
Emilio Romo <natsutsu04@gmail.com>
2023-05-30 11:04:25 -0600

    mensaje

--- exercises.py
+++ exercises.py
@@ -23,3 +23,18 @@ while index < len(tuple):
 #Now print the elements 
     print(tuple[index])
     index + = 1
+#We print the default data
+print("Civilian data:")
+print("Name: Samuel" )
+print("Nationality: Boliviano")
+print("Age: 18")
+#Now we introduce the new data
+name = input("Enter the new name: ")
+nationality = input("Enter the new nationality: ")
+age = int(input("Enter the new age: "))
+#Now the code will show the new data that has been entered
+print("Final data:")
+print("Name:", name)
+print("Nationality:", nationality)
+print("Age:", age)
+
